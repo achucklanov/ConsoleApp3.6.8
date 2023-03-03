97cff0bff821a8c8a7da1bdcd2e66261f0a005da
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-03 21:07:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3.6.8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp3._6._8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine("10 % 3 = {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
